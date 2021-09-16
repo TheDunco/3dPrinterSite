ee5c0a67f90b396af59648cf5dbffefdd5f68e11
TheDunco <duncvan@gmail.com>
2021-09-16 10:11:48 -0400

    Added pages and deleted template

--- InertiaPrinterSite/src/app/app.module.ts
+++ InertiaPrinterSite/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { FilesComponent } from './pages/files/files.component';
+import { RecentsComponent } from './pages/recents/recents.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    DashboardComponent,
+    FilesComponent,
+    RecentsComponent
   ],
   imports: [
     BrowserModule,
